f326422adabc53efb90aafa2bab54daf77e910f0
Sirius <30409371+KuiperSirius@users.noreply.github.com>
2017-11-08 19:57:11 +0800

    Update Main.java

--- Matrix_ TwoDimensionalArrays/src/Main.java
+++ Matrix_ TwoDimensionalArrays/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 		 //倘若矩阵（二维数组）在main()方法外声明，则"不能对非静态字段进行静态引用"，控制台报错。
       int[][] expMatrix={{2,4,56,78,55},{41,45,77},{5,99,46,26},{46,55,15}};
       int[][] expMatrix_b={{25,25,5,78,55},{1,45,757,59},{51,92,46,26},{6,12,15}};
+		//讲二维数组写入矩阵，多余丢弃，缺失补零。
 		Matrix testMatrix=new Matrix(4, 4, expMatrix);
 		Matrix testMatrix_b=new Matrix(4, 4, expMatrix_b);
 		//检验算法是否正确
@@ -16,7 +17,6 @@ public class Main {
 			System.out.println("测试矩阵为对称矩阵.");
 		}else
 			System.out.println("测试矩阵不为对称矩阵."); **/
-	
 		System.out.println("原测试矩阵一为："+str_1);
 		System.out.println("原测试矩阵二为："+str_2);
 		System.out.println("矩阵和为："+testMatrix.add(testMatrix_b).toString());
