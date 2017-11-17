e681c20f6f544c5dd8daf6e6c12c7c93a6503ebb
Sirius <gyh1298058589@gmail.com>
2017-11-17 21:16:04 +0800

    Update Main_1.java

--- BinaryTreeInfo/src/Main_1.java
+++ BinaryTreeInfo/src/Main_1.java
@@ -9,9 +9,7 @@ public class Main_1<T> {
 	
 	
 	public static void main(String[] args) {	
-		String str_1="ABCDFEG";
-	
-		
+		String str_1="AbcDEFG";		
 	/*	//定义输入输出流，获取键盘输入字符串。	
 		InputStreamReader stdin = new InputStreamReader(System.in);//键盘输入 
 		BufferedReader bufin = new BufferedReader(stdin); 
@@ -20,18 +18,18 @@ public class Main_1<T> {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
-        */
-		
+        */	
 		String[] strArray=new String[str_1.length()];
 		for(int i=0;i<str_1.length();i++){
 		strArray[i]=String.valueOf(str_1.charAt(i));
 		}
 		//构造方法生成数组方式，链式存储二叉树层序结构。
-		BinaryTree<String> tree=new BinaryTree<String>(strArray);
-		
-     //   tree.preorder(); 	
+		BinaryTree<String> tree=new BinaryTree<String>(strArray);		
+        //tree.preorder(); 	
 		//System.out.println(tree.levelorder()); 
-		tree.inorder();
+		//tree.inorder();
+		
+		System.out.println(tree.levelorder());
 	}
 
 }
