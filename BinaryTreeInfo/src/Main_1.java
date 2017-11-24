c2ac3da418fc8b2e7d7010fa17dd8d3d7fe44927
Ursa Minor <gyh1298058589@gmail.com>
2017-11-24 17:45:46 +0800

    Update Main_1.java

--- BinaryTreeInfo/src/Main_1.java
+++ BinaryTreeInfo/src/Main_1.java
@@ -8,7 +8,7 @@ public class Main_1<T> {
 	
 	
 	
-	public static void main(String[] args) {	
+	public static void main(String[] args){	
 		String str_1="AbcDEFG";		
 	/*	//定义输入输出流，获取键盘输入字符串。	
 		InputStreamReader stdin = new InputStreamReader(System.in);//键盘输入 
@@ -28,9 +28,20 @@ public class Main_1<T> {
         //tree.preorder(); 	
 		//System.out.println(tree.levelorder()); 
 		//tree.inorder();
+		System.out.println("测试原始二叉树层序输出");
+		System.out.println(tree.levelorder());//测试原始二叉树层序输出
+		System.out.println("测试非递归后序输出");
+		tree.postOrder(tree.root);
+		System.out.println("测试镜像二叉树层序输出");
+		tree.mirrorTree(tree.root);//更改镜像二叉树根节点
+		System.out.println(tree.levelorder());//测试镜像二叉树层序输出
 		
-		System.out.println(tree.levelorder());
+		Integer[] preSort={1,2,4,5,3,6};//先序序列
+		Integer[] inSort={4,2,5,1,6,3};//中序序列*/
+	
+		BinaryTree<Integer> tree_1=new BinaryTree<Integer>(preSort,inSort);
+		System.out.println(tree_1.levelorder());
+		
 	}
 
 }
-//@author：Sirius 2017-11-17
